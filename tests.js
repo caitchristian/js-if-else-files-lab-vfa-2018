70723e80fde444a51c13e180f5350c457e2a217f
Caitlin Christian <caitycec@gmail.com>
2018-07-01 02:38:01 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -3,7 +3,7 @@ var index = require("./index.js")
 if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
-    console.log("The name is correct"+index.name)
+    console.log("The name is correct")
 }
 
 
@@ -11,5 +11,5 @@ if (index.name === "Susan") {
 if (index.height === 70) {
     console.log("The height is correct")
 } else {
-    console.log("The height is correct")} "+index.height)
+    console.log("The height is correct")
 }
